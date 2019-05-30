f06c382bca9094c50186801471b07d39119725da
Ubuntu <ubuntu@ERC20token.0cuoe1ywnu3uzc45zztqd50hjh.ax.internal.cloudapp.net>
2019-05-30 15:09:13 +0000

    Fixed post excel to api

--- routes/download_excel.js
+++ routes/download_excel.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.redirect('http://52.142.220.82:8180/poc-intercompany-1/download');
+    res.redirect('http://52.142.199.184:8080/poc-intercompany-1/download');
 });
 
 module.exports = router;
